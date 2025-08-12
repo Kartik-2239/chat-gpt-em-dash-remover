bb9fa1b27944dc6d473f806678115bbdeb7b8216
Kartik <kartikkannan22@gmail.com>
2025-08-12 16:17:07 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,41 +1,12 @@
-
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.action.setBadgeText({
-//     text: "OFF",
-//   });
-// });
-
 const chatGPT = 'https://chatgpt.com/c/';
 
 
 chrome.action.onClicked.addListener(async (tab) => {
   if (tab.url.startsWith(chatGPT)) {
-    // const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-
-    // await chrome.action.setBadgeText({
-    //   tabId: tab.id,
-    //   text: nextState,
-    // });
-
-    // if (nextState === "ON") {
-
-    //   await chrome.scripting.insertCSS({
-    //     files: ["focus-mode.css"],
-    //     target: { tabId: tab.id },
-    // })
-
+    
     await chrome.scripting.executeScript({
         target :{tabId: tab.id},
         files : ["script.js"],
     });
-
-    // } else if (nextState === "OFF") {
-    //   // Remove the CSS file when the user turns the extension off
-    //   await chrome.scripting.removeCSS({
-    //     files: ["focus-mode.css"],
-    //     target: { tabId: tab.id },
-    //   });
-    // }
   }
 });
